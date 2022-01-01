abe15f0fcf0376f567f4108363e5843558fa6a7b
Enzo Guido <enzo192@gmail.com>
2022-01-01 14:23:16 -0500

    agregador el programa de salarios, el index en donde se unen todos los programas

---
+++ analisis.js
@@ -0,0 +1,73 @@
+// helpers
+function esPar(numero) {
+    return (numero % 2 === 0);
+ };
+
+
+function calcularMediaAritmetica(lista) {
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    )
+
+    const promedioLista = (sumaLista / lista.length);
+
+    return promedioLista;
+}
+
+// Calculadora de Medianas
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
+
+        return mediana;
+
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+};
+
+// Mediana general
+
+const salarioCol = colombia.map(
+    function (persona) {
+        return persona.salary;
+    }
+);
+
+const salariosColSorted = salarioCol.sort(
+    function (A, B) {
+        return A - B;
+    }
+);
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+// Mediana del top 10%
+
+
+const spliceStart = (((salariosColSorted.length) * (100 - 10))/100);
+const spliceCount = salariosColSorted.length - spliceStart;
+
+const salariosColTop10 = salariosColSorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+
+
+console.log(
+    medianaGeneralCol,
+    medianaTop10Col,
+);
---
+++ salarios.js
@@ -0,0 +1,82 @@
+const colombia = [];
+
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Fernanda",
+    salary: 600,
+});
+colombia.push({
+    name: "Camilo",
+    salary: 700,
+});
+colombia.push({
+    name: "Fernando",
+    salary: 800,
+});
+colombia.push({
+    name: "Jorge",
+    salary: 200,
+});
+colombia.push({
+    name: "Clara",
+    salary: 300,
+});
+colombia.push({
+    name: "Maria",
+    salary: 400,
+});
+colombia.push({
+    name: "Rafa",
+    salary: 1000,
+});
+colombia.push({
+    name: "Ruddy",
+    salary: 500,
+});
+colombia.push({
+    name: "Sharol",
+    salary: 100,
+});
+colombia.push({
+    name: "stefany",
+    salary: 900,
+});
+colombia.push({
+    name: "Michael",
+    salary: 700,
+});
+colombia.push({
+    name: "Juan",
+    salary: 1500,
+});
+colombia.push({
+    name: "Pedro",
+    salary: 1500,
+});
+colombia.push({
+    name: "Natalia",
+    salary: 1000,
+});
+colombia.push({
+    name: "Esperanza",
+    salary: 600,
+});
+colombia.push({
+    name: "Mayerly",
+    salary: 300,
+});
+colombia.push({
+    name: "Brian",
+    salary: 300,
+});
+colombia.push({
+    name: "Carlos",
+    salary: 400,
+});
+colombia.push({
+    name: "Rodolfo",
+    salary: 500,
+});
